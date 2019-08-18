05d18fb64f0049c01ac794aab0c66c04c4b155bf
Jared Allard <jaredallard@outlook.com>
2019-08-17 19:55:40 -0700

    feat(main): support rabbitmq from env var

--- main.go
+++ main.go
@@ -70,7 +70,13 @@ func main() {
 		providers[p] = provider
 	} // for loop end
 
-	client, err := rabbitmq.NewClient("amqp://user:bitnami@127.0.0.1:5672")
+	amqpEndpoint := os.Getenv("IDENTIFIER_RABBITMQ_ENDPOINT")
+	if amqpEndpoint == "" {
+		amqpEndpoint = "amqp://user:bitnami@127.0.0.1:5672"
+		log.Warnf("TWILIGHT_RABBITMQ_ENDPOINT not defined, defaulting to local config: %s", amqpEndpoint)
+	}
+
+	client, err := rabbitmq.NewClient(amqpEndpoint)
 	if err != nil {
 		log.Fatalf("failed to connect to rabbitmq: %v", err)
 	}
