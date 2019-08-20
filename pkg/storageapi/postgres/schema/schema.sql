210c3121552d1198d875f616345ef0df5b9fb58c
Jared Allard <jaredallard@outlook.com>
2019-08-19 21:01:43 -0700

    fix: initialize episodes_v1 before series_v1

--- pkg/storageapi/postgres/schema/schema.sql
+++ pkg/storageapi/postgres/schema/schema.sql
@@ -15,6 +15,30 @@ COMMENT ON COLUMN episode_files_v1.quality IS 'Quality of this file, valid optio
 COMMENT ON COLUMN episode_files_v1.created_at IS 'When this file was added';
 
 --------------------
+-- EPISODES
+--------------------
+CREATE TABLE episodes_v1 (
+    id character varying(128) PRIMARY KEY,
+    media_id character varying(128) NOT NULL,
+    absolute_number integer NOT NULL,
+    description text NOT NULL,
+    air_date date,
+    season double precision NOT NULL,
+    season_number double precision NOT NULL,
+    created_at timestamp with time zone DEFAULT now(),
+    UNIQUE (media_id, season, season_number),
+    UNIQUE (media_id, absolute_number)
+);
+COMMENT ON COLUMN episodes_v1.id IS 'ID this episode, a UUID';
+COMMENT ON COLUMN episodes_v1.media_id IS 'ID of the media this episode belong too';
+COMMENT ON COLUMN episodes_v1.absolute_number IS 'Absolute episode number';
+COMMENT ON COLUMN episodes_v1.description IS 'Synopsis, or description, of this episode';
+COMMENT ON COLUMN episodes_v1.air_date IS 'When this episode first aired, this is only acurrate to the MM-DD-YYYY';
+COMMENT ON COLUMN episodes_v1.created_at IS 'When this episode was added';
+COMMENT ON COLUMN episodes_v1.season IS 'Season this episode was in';
+COMMENT ON COLUMN episodes_v1.season_number IS 'Number this episode was in a season';
+
+--------------------
 -- SERIES DATA
 --------------------
 CREATE TABLE series_v1 (
@@ -44,30 +68,6 @@ COMMENT ON COLUMN series_v1.genres IS 'CSV list of genres this media is';
 COMMENT ON COLUMN series_v1.airs IS 'HH:MM time that this show airs';
 COMMENT ON COLUMN series_v1.air_day_of_week IS 'Day of the week this show airs';
 COMMENT ON COLUMN series_v1.runtime IS 'Runtime of this media on average';
-
---------------------
--- EPISODES
---------------------
-CREATE TABLE episodes_v1 (
-    id character varying(128) PRIMARY KEY,
-    media_id character varying(128) NOT NULL,
-    absolute_number integer NOT NULL,
-    description text NOT NULL,
-    air_date date,
-    season double precision NOT NULL,
-    season_number double precision NOT NULL,
-    created_at timestamp with time zone DEFAULT now(),
-    UNIQUE (media_id, season, season_number),
-    UNIQUE (media_id, absolute_number)
-);
-COMMENT ON COLUMN episodes_v1.id IS 'ID this episode, a UUID';
-COMMENT ON COLUMN episodes_v1.media_id IS 'ID of the media this episode belong too';
-COMMENT ON COLUMN episodes_v1.absolute_number IS 'Absolute episode number';
-COMMENT ON COLUMN episodes_v1.description IS 'Synopsis, or description, of this episode';
-COMMENT ON COLUMN episodes_v1.air_date IS 'When this episode first aired, this is only acurrate to the MM-DD-YYYY';
-COMMENT ON COLUMN episodes_v1.created_at IS 'When this episode was added';
-COMMENT ON COLUMN episodes_v1.season IS 'Season this episode was in';
-COMMENT ON COLUMN episodes_v1.season_number IS 'Number this episode was in a season';
 
 --------------------
 -- IMAGES
